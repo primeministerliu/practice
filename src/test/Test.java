08b06c3ac43917a4c0d442a22d7da9d7da14e1b2
primeministerliu <872712106@qq.com>
2018-12-23 16:30:37 +0800

    测试

---
+++ src/test/Test.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.net.URLDecoder;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+class A {}
+public class Test extends Thread {
+
+	//剪艺造型
+	public static void main(String[] args) throws Exception {
+		/*Random random = new Random();
+		System.out.println(random.nextInt(3)+1);
+		System.out.println((int)(Math.random()*3+1));
+		HashMap<String,List> map;
+		Map<String,String> map1 = new HashMap();
+		
+		for (int i = 0; i <10; i++) {
+			final long end= System.currentTimeMillis()+60*60*1000;
+			
+			System.out.println(new Date(new Date().getTime()+60*60*1000).toLocaleString());
+			System.out.println(end);
+			System.out.println();
+		
+		}*/
+		
+	System.out.println(10%9);
+	}
+
+}
